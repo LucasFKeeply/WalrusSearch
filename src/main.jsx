0b27114dc0c71347f97745858a0d650aef718a45
lucasfffffffffffff <lucasforeman@keeplyai.com>
2025-10-31 11:08:18 +0000

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createHashRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 
 import App from './App.jsx'
 import PrivacyPolicy from './privacy-policy.jsx'
 import TermsOfService from './terms-of-service.jsx'
 
-const router = createHashRouter([
+const router = createBrowserRouter([
   { path: '/', element: <App /> },
   { path: '/privacy', element: <PrivacyPolicy /> },
   { path: '/terms', element: <TermsOfService /> },
